4c01f6ec48a37c5141833ed86dd386566e8b71c6
DMK <dominiks8318@gmail.com>
2023-03-11 17:08:14 +0100

    Added check for mute punishment.

--- src/main/java/me/dmk/core/listener/PrivateMessageListener.java
+++ src/main/java/me/dmk/core/listener/PrivateMessageListener.java
@@ -4,11 +4,16 @@ import lombok.AllArgsConstructor;
 import me.dmk.core.chat.notification.NotificationController;
 import me.dmk.core.event.PrivateMessageEvent;
 import me.dmk.core.profile.Profile;
+import me.dmk.core.profile.punishment.Punishment;
+import me.dmk.core.profile.punishment.PunishmentType;
 import me.dmk.core.profile.settings.ProfileSettings;
+import me.dmk.core.util.TimeUtil;
 import me.dmk.core.util.string.StringFormatter;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+
+import java.util.Optional;
 
 /**
  * Created by DMK on 13.02.2023
@@ -29,6 +34,13 @@ public class PrivateMessageListener implements Listener {
 
         Profile receivingProfile = event.getReceivingProfile();
         ProfileSettings receivingSettings = receivingProfile.getProfileSettings();
+
+        Optional<Punishment> punishment = senderProfile.getActivePunishment(PunishmentType.MUTE);
+        if (punishment.isPresent()) {
+            event.setCancelMessage("<red>Wyciszono cię <dark_gray>- <red>wygasa <gold>" + (punishment.get().isPermanent() ? "nigdy" : "za " + TimeUtil.instantToString(punishment.get().getExpireAt().toInstant(), true)) + "<dark_gray>.");
+            event.setCancelled(true);
+            return;
+        }
 
         if (sender.getUniqueId().equals(receiving.getUniqueId())) {
             event.setCancelMessage("<red>Nie możesz wysyłać prywatnych wiadomości do samego siebie<dark_gray>.");
