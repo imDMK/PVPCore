db3383bff511be062cb88437dcf24237197d9328
DMK <dominiks8318@gmail.com>
2023-03-15 22:34:51 +0100

    Code cleanup.

--- src/main/java/me/dmk/core/command/argument/guild/GuildArgument.java
+++ src/main/java/me/dmk/core/command/argument/guild/GuildArgument.java
@@ -43,7 +43,7 @@ public class GuildArgument implements OneArgument<Guild> {
     @Override
     public List<Suggestion> suggest(LiteInvocation invocation) {
         return Suggestion.of(
-                this.guildController.getStringGuildCache().asMap().keySet()
+                this.guildController.getTagGuildCache().asMap().keySet()
         );
     }
 }
